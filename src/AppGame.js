5d6bc58165b0f03a0902d5ca696661721fa9f043
JeffreyWong05 <jeffjeff217@hotmail.com>
2023-12-21 00:38:50 -0400

    updated padding on tetris page

--- src/AppGame.js
+++ src/AppGame.js
@@ -28,7 +28,7 @@ const features = [
 const AppGame = () => (
     <div className="bg-[url('https://cdn.discordapp.com/attachments/1029635450895339603/1150339413570768907/falling-tetrominos-pixilart_scaled_40x_pngcrushed.png')] bg-[length:700px_817px]">
 
-        <div className="overflow-hidden py-24 sm:py-32">
+        <div className="overflow-hidden py-5 sm:py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
